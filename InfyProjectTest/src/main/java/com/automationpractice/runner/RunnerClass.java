88c72559ec71dc7af8044e8ad38c79103be6baf9
HP <HP@LAPTOP-6EOENUHD>
2022-08-30 16:27:07 +0530

    commit---1

--- InfyProjectTest/src/main/java/com/automationpractice/runner/RunnerClass.java
+++ InfyProjectTest/src/main/java/com/automationpractice/runner/RunnerClass.java
@@ -8,10 +8,9 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features="D:\\Practice Workspace\\InfyProjectTest\\src\\main\\java\\Features",
-		glue= {"stepdefinations"},
+		glue= {"stepdefinations"}
 		strict = true,
-		dryRun = false,
-		tags = "(@tag1, @tag2)"
+		dryRun = false
 	
 		)
 
